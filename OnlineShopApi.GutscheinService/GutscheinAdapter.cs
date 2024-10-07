7a2bde230ec4d1738110be70b9d96eea4b7ce1b3
Kriss <kipkekriss@gmail.com>
2024-10-07 23:30:39 +0200

    Grpc

--- OnlineShopApi.GutscheinService/GutscheinAdapter.cs
+++ OnlineShopApi.GutscheinService/GutscheinAdapter.cs
@@ -1,7 +1,12 @@
-﻿namespace OnlineShopApi.GutscheinService
-{
-    public class GutscheinAdapter
-    {
+﻿using Grpc.Core;
+using OnlineShopApi.GutscheinService.Contracts;
 
+namespace OnlineShopApi.GutscheinService;
+
+public class GutscheinAdapter : GutscheinService.Contracts.GutscheinService.GutscheinServiceBase
+{
+    public override Task<HoleGutscheinMitIdResponse> HoleGutscheinMitId(HoleGutscheinMitIdRequest request, ServerCallContext context)
+    {
+        return base.HoleGutscheinMitId(request, context);
     }
 }
